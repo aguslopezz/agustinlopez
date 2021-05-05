2f0051b4e174e31c792562b81bbadc786fff4922
aguslopezz <72411830+aguslopezz@users.noreply.github.com>
2021-05-05 01:13:47 -0300

    Organizando

--- Laboratorio/Java/Main19abrilv2.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-public class Main19abrilv2 {
-    public static void main(String[] args) {
-        SistemadeProductos19abril system= new SistemadeProductos19abril();
-        system.iniciarSistema();
-    }
-
-}
