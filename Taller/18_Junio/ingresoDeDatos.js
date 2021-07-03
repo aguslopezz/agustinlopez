a52903e524fd08ca20a7111b8fd49cac14f37065
aguslopezz <72411830+aguslopezz@users.noreply.github.com>
2021-07-03 19:49:09 -0300

    Actualizando

---
+++ Taller/18_Junio/ingresoDeDatos.js
@@ -0,0 +1,14 @@
+
+    function mostrardatos(){
+
+    let mensaje1 = $("#nombre").val();
+    let mensaje2 = $("#apellido").val();
+    let mensaje3 = $("#edad").val();
+    
+    $("#etiqueta1").text(mensaje1);
+    $("#etiqueta2").text(mensaje2);
+    $("#etiqueta3").text(mensaje3);
+
+    
+
+    }
