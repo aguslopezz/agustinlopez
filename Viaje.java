3e0f9429bf5e9d3698aad1a7bce64344c6d532b3
aguslopezz <72411830+aguslopezz@users.noreply.github.com>
2021-07-14 11:22:21 -0300

    Add files via upload

---
+++ Viaje.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Viaje {
+    float precio;
+    String fecha;
+    String hora;
+
+    public Viaje(float precio, String fecha, String hora){
+        this.precio = precio;
+        this.fecha = fecha;
+        this.hora = hora;
+
+    }
+}
