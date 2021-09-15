a8a238d57d158e693a3dc6474024985c9ca00449
aguslopezz <72411830+aguslopezz@users.noreply.github.com>
2021-09-15 11:40:36 -0300

    Add files via upload

---
+++ company/Artista.java
@@ -0,0 +1,13 @@
+package com.company;
+
+public class Artista {
+    public String nombre;
+    public String apellido;
+    public String fechaNacimiento;
+
+    public Artista(){
+        this.nombre = "Dababy";
+        this.apellido = "Roger";
+        this.fechaNacimiento = "23-07-1987";
+    }
+}
---
+++ company/Autor.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Autor extends Artista{
+    public String nacionalidad;
+
+    public Autor(){
+        super();
+        this.nacionalidad = "Argentino";
+
+    }
+}
