06103f286f6d3176c010b479d8157c3c94ce085e
aguslopezz <72411830+aguslopezz@users.noreply.github.com>
2021-11-26 14:07:17 -0300

    Delete SistemaDeVentas.java

--- company/SistemaDeVentas.java
+++
@@ -1,7 +0,0 @@
-package com.company;
-
-public interface SistemaDeVentas {
-    public float gananciaTotalObtenida();
-    public float ingresosTotales();
-    public int cantidadDeProductosVendidos();
-}
